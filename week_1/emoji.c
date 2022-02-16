42d30f4daa77b3bda4c4f8535810a233b472ce69
suragch <studymongolian@gmail.com>
2022-02-16 10:19:05 +0800

    Create emoji.c

---
+++ week_1/emoji.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void) {
+    printf("hello my name is Bob\n");
+    printf("I am 20 years old.\n");
+    printf("I come from America.\n");
+
+    printf("  ⊂_ヽ\n");
+    printf("　 ＼＼\n");
+    printf("　　 ＼( ͡° ͜ʖ ͡°)\n");
+    printf("　　　 >　⌒ヽ\n");
+    printf("　　　/ 　 へ＼\n");
+    printf("　　 /　　/　＼＼\n");
+    printf("　　 ﾚ　ノ　　 ヽ_つ\n");
+    printf("　　/　/\n");
+    printf("　 /　/|\n");
+    printf("　(　(ヽ\n");
+    printf("  |　|、＼\n");
+    printf("　| 丿 ＼ ⌒)\n");
+    printf("　| |　　) /\n");
+    printf("ノ )　　Lﾉ\n");
+    printf("(_／\n");
+    return 0;
+}
